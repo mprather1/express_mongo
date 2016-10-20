fea2437a67624877ced048fa9d38bd628db99a80
Michael Prather <mprather1@gmail.com>
2016-10-19 20:02:54 -0500

    post users

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/api');
+var User = require("./app/models/user");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8000;
+
+var router = express.Router();
+
+router.use(function(req, res, next){
+  console.log("Something is happening....");
+  next();
+});
+
+router.get('/', function(req, res){
+  res.json({ message: "Hello"});
+});
+
+router.route('/users')
+  .post(function(req, res){
+    var user = new User();
+    user.name = req.body.name;
+    user.save(function(err){
+      if (err)
+        res.send(err);
+      res.json({ message: "User created..." });
+    });
+  });
+
+app.use('/api', router);
+
+var server = app.listen(port, function(){
+  console.log(`Listening on port ${port}...`);
+});
